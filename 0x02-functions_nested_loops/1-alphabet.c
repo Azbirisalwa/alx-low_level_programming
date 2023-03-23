58b54134db3ad7de9987450fbaceb755f4bd9a7f
Azbirisalwa <azbirisalwa@gmail.com>
2023-03-22 17:01:11 -0700

    I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabets in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char lettre;
+
+	for (lettre = 'a'; lettre <= 'z'; lettre++)
+	{
+		_putchar(lettre);
+	}
+	_putchar('\n');
+}
