326ad3a16195e7761949dda3df1d5f794c774f91
Azbirisalwa <azbirisalwa@gmail.com>
2023-03-27 17:47:43 -0700

    bety fixed

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,24 +1,21 @@
 #include "main.h"
 
 /**
- * _strcpy - coples the string pointed to by src
- * @dest: char type string
- * @src: char type string
- * desciption: copy the string pointed to by pointer 'src' to 
- * the buffer pointed to by 'dest'
- * Return: pointer to 'dest'
+ *_strcpy - coples the string pointed to by src
+ *@dest: char type string
+ *@src: char type string
+ *desciption: copy the string pointed to by pointer 'src' to
+ *the buffer pointed to by 'dest'
+ *Return:pointer to 'dest'
  */
 
 char *_strcpy(char *dest, char *src)
-{
+	{
 	int z = -1;
 
-	do 
-	{
+	do {
 		z++;
-		dest [z] = src [z];
-	}
-	while ( src[z]!= '\0');
-	
+		dest[z] = src[z];
+	} while (src[z] != '\0');
 	return (dest);
-}
+	}
