0fa75e66396d447b4b1736629b7558fd561c080c
Azbirisalwa <azbirisalwa@gmail.com>
2023-04-05 16:44:10 -0700

    0from6tasks

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,17 @@
+#ifndef _main_h_
+#define _main_h_
+
+void _puts_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _strlen_recursion(char *s);
+int _sqrt_recursion(int n);
+int is_palindrome(char *s);
+int _sqrt_subfunction(int n, int y);
+void _print_rev_recursion(char *s);
+int prime_sub(int n, int r);
+int is_prime_number(int n);
+int _putchar(char c);
+int wildcmp(char *s1, char *s2);
+
+#endif
